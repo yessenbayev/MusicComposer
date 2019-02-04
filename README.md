701229016b31290128fada0e19f8830884632445
Alan' Yessenbayev <alan.yessenbayev@gmail.com>
2019-02-03 21:42:39 -0800

    wrote a web-crawler for the dataset

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ece271B
+## Downloading the dataset
+run 
+conda install requests
+conda install -c anaconda beautifulsoup4
+then run
+python download_dataset.py
---
+++ download_dataset.py
@@ -0,0 +1,81 @@
+import requests 
+import os
+import re
+from bs4 import BeautifulSoup 
+  
+''' 
+URL of the archive web-page which provides link to 
+all video lectures. It would have been tiring to 
+download each video manually. 
+In this example, we first crawl the webpage to extract 
+all the links and then download videos. 
+'''
+  
+# specify the URL of the archive here 
+archive_url = "https://www.vgmusic.com"
+
+def needed_links(href):
+    return href and re.compile("/music/").search(href)
+
+def get_audio_links(): 
+
+
+      
+    # create response object 
+    r = requests.get(archive_url) 
+      
+    # create beautiful-soup object 
+    soup = BeautifulSoup(r.content,'html5lib') 
+      
+    # find all links on web-page 
+    links = soup.findAll('a',href=needed_links) 
+    audio_links = []
+
+    for link in links:
+        r2 = requests.get(archive_url+link['href'].strip('.')) 
+        soup = BeautifulSoup(r2.content,'html5lib') 
+        download_links = soup.findAll('a',href=True) 
+    # filter the link sending with .mid
+        audio_links += [archive_url + link['href'].strip('.') + download_link['href'] for download_link in download_links if download_link['href'].endswith('mid')] 
+    return audio_links 
+  
+  
+def download_audio_series(audio_links,directory): 
+  
+    for link in audio_links: 
+  
+        '''iterate through all links in audio_links 
+        and download them one by one'''
+          
+        # obtain filename by splitting url and getting  
+        # last string 
+        file_name = link.split('/')[-1]    
+  
+        print("Downloading file:%s"%file_name)
+          
+        # create response object 
+        r = requests.get(link, stream = True) 
+          
+        # download started 
+        with open(os.path.join(directory,file_name), 'wb') as f: 
+            for chunk in r.iter_content(chunk_size = 1024*1024): 
+                if chunk: 
+                    f.write(chunk) 
+          
+        print("%s downloaded!\n"%file_name) 
+  
+    print("All audios downloaded!")
+    return
+  
+  
+if __name__ == "__main__": 
+    directory = 'data'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+  
+    # getting all video links 
+    print("Finding all links on the website")
+    audio_links = get_audio_links() 
+  
+    # download all videos 
+    download_audio_series(audio_links,directory) 
