40889c101494053900c2d4210cd8731c849c0c8a
Eason Chi <eason@Easons-MacBook-Pro.local>
2019-02-28 22:48:38 -0800

    add DataGenerator.py, midi.py

---
+++ DataGenerator.py
@@ -0,0 +1,58 @@
+from os import walk, path
+from collections import deque
+import numpy as np
+
+
+class MidiDataGenerator():
+	def __init__(self, root_dir, m=16):
+		self.root_dir = root_dir
+		self.paths = []
+		for root, _, files in walk(self.root_dir):
+			for file in files:
+				if file.endswith('.npy'):
+					self.paths.append(path.join(root, file))
+
+		self.paths.sort()
+		self.queue = deque()
+		self.pos = 0
+		self.len = m
+
+	def format_data(self, data):
+		m_i = data.shape[0]
+		if m_i > self.len:
+			tmpArr = np.vstack([
+				data, 
+				np.zeros((self.len - m_i % self.len,96,96), dtype=np.uint8)
+			]) if m_i % self.len else data
+			return np.vsplit(tmpArr, tmpArr.shape[0] // self.len)
+		
+		# in case when m_i < m
+		return [np.vstack([
+			data for k in range(self.len // m_i)
+		])] if not self.len % m_i \
+		else [np.vstack([
+			np.vstack([data for k in range(self.len // m_i)]),
+			np.zeros((self.len % m_i,96,96), dtype=np.uint8)
+		])]
+
+	def samples(self, size=10):
+		while len(self.queue) < size:
+			data = np.load(self.paths[self.pos])
+			self.pos = (self.pos + 1) % len(self.paths)
+			for sample in self.format_data(data):
+				self.queue.append(sample)
+
+		return np.array([
+			self.queue.popleft() for k in range(size)
+		])
+
+
+
+
+
+
+
+
+
+
+
---
+++ midi.py
@@ -0,0 +1,91 @@
+from mido import MidiFile, MidiTrack, Message
+import numpy as numpy
+
+## the desired output feature shape
+number_of_notes = 96
+samples_per_measure = 96
+
+## desition
+working_directory = './dataset'
+saving_directory = './raw'
+
+
+## Convert MIDI files to samples
+def midi_to_samples(filename, info=None):
+	mid = MidiFile(filename)
+	if mid.type==2: raise AsynchronousTracks
+
+	signature = [
+		(msg.numerator, msg.denominator) 
+		for track in mid.tracks 
+		for msg in track 
+		if msg.is_meta and msg.type == 'time_signature'
+	]
+
+	if not signature: seg = (4, 4)
+	else:
+		seg = signature[0]
+		for k in range(1, len(signature)):
+			if seg!=signature[k]: 
+				seg = None
+				break 
+
+	if not seg: raise TimeSignatureError
+
+	ticks_per_sample = int(mid.ticks_per_beat * seg[0] / samples_per_measure)
+	if not ticks_per_sample: raise ZeroDivisionError
+
+	Seq = [[ ] for j in range(number_of_notes)]
+	max_k = 0
+	for track in mid.tracks:
+		tick = 0
+		for msg in track:
+			tick += msg.time
+			if msg.is_meta: continue 
+			elif msg.type=='note_on' and msg.velocity:
+				k = tick//ticks_per_sample
+				n = msg.note - 21
+				if k not in Seq[n]: Seq[n].append(k)
+				if k > max_k: max_k = k
+
+	d = max_k // samples_per_measure + 1
+
+	samples = np.zeros((number_of_notes, d * samples_per_measure), dtype=np.uint8)
+
+	for k, seq in enumerate(Seq):
+		if not seq: continue
+		for idx in seq:
+			samples[k, idx] = 1
+
+	samples = np.hsplit(samples, d)
+
+	directory = filename.split('/')[-2]
+	if not os.path.exists(os.path.join(saving_directory, directory)):
+		os.makedirs(os.path.join(saving_directory, directory))
+
+	np.save(os.path.join(saving_directory, directory, filename.split('/')[-1]), samples)
+
+
+def samples_to_midi(samples, filename, thresh=0.5):
+	mid = MidiFile()
+	track = MidiTrack()
+	mid.tracks.append(track)
+
+	ticks_per_sample = 4 * mid.ticks_per_beat / samples_per_measure
+
+	last_ticks, ticks = 0, 0
+	for k, sample in enumerate(samples):
+		for y in range(sample.shape[0]):
+			ticks += ticks_per_sample
+			for x in range(sample.shape[1]):
+				note = x + 21
+				# determine if the node on
+				if sample[y, x] >= thresh and (y==0 or sample[y-1,x] < thresh):
+					track.append(Message('note_on',  note=note, velocity=127, time=ticks-last_ticks))
+					last_ticks = ticks
+				if sample[y, x] >= thresh and (y==sample.shape[0]-1 or sample[y+1,x] < thresh):
+					track.append(Message('note_off', note=note, velocity=127, time=ticks-last_ticks))
+					last_ticks = ticks
+
+
+	mid.save(filename)
